7760c1082390852806d03139ba076d1c058e1aa0
SudoCpp <SudoCpp@gmail.com>
2021-05-11 22:25:00 -0500

    Experimenting with Struc

--- main.asm
+++ main.asm
@@ -8,6 +8,12 @@ include 'mem/variableStack.inc'
 
 segment readable executable
 
+struc twoStrings first,last
+{
+    .first char first
+    .last char last
+}
+
 entry start    ; in the original file this was located before the segment. You should put it here instead
 
 start:
@@ -15,8 +21,14 @@ start:
     initStack
 
     salloc 10, bufferAddress
-    readConsole [bufferAddress], 10
-    writeConsole [bufferAddress], 10
+    ;readConsole [bufferAddress], 10
+    ;writeConsole [bufferAddress], 10
+    
+    readConsole structTest.first, 10
+    readConsole structTest.last, 10
+    writeConsole structTest.first, 10
+    writeConsole structTest.last, 10
+    
     
     deinitHeap
     SCall1 SC_exit, 0
@@ -25,3 +37,4 @@ start:
 segment readable writeable ; basically .bbs
 
 bufferAddress int32 0
+structTest twoStrings '          ', '          '
